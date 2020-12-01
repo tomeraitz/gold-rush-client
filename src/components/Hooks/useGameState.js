b474e5acf63bd270c6d6f73eb5ab84e079a3cffd
Tomer Raitz <tomeraitz1@gmail.com>
2020-12-01 16:13:12 +0200

    fixed the phone buttons bug

--- src/components/Hooks/useGameState.js
+++ src/components/Hooks/useGameState.js
@@ -4,15 +4,10 @@ import {
    useCallback,
 } from 'react'
 import * as io from 'socket.io-client'
-import { useSwipeable } from "react-swipeable";
 import DeviceDetector from "device-detector-js";
 const deviceDetector = new DeviceDetector();
 const device = deviceDetector.parse(navigator.userAgent);
-let toucheEvent = {
-   dir : '',
-   counter : 0
-};
-
+let endgame = false;
 const keyCodeList = {
    38 : 'moveUp',
    39: 'moveRight',
@@ -20,11 +15,13 @@ const keyCodeList = {
    37 : 'moveLeft'
 }
 
+
 const useGameState = () => {
    const [player, setPlayer] = useState('player1');
    const [data, setData] = useState({});
    const [socket, setSocket] = useState(null);
    const [timeoutInterval , setTimeoutInterval] = useState(null);
+  
 
    const handleUserKeyPress = useCallback(event => {
       const {
@@ -45,16 +42,17 @@ const useGameState = () => {
    }
 
    const startPress = (e) =>{
-      if (e.changedTouches.length > 1) e.preventDefault();
-      e.keyCode = e.target.id*1 || e.target.parentElement.id*1;
+      if(endgame){
+         endPress();
+         return;
+      }
+      e.keyCode = e.target ? (e.target.id*1 || e.target.parentElement.id*1) : 0;
       if(!timeoutInterval){
+         handleUserKeyPress(e);
          setTimeoutInterval(
             setTimeout(() => {
-               if(data.gridArray){
-                  handleUserKeyPress(e);
-                  startPress(e);
-               }
-            }, 70)
+              startPress(e);
+            }, 150)
          )
       }
    }
@@ -73,6 +71,7 @@ const useGameState = () => {
             if(isDesktop()) window.addEventListener('keydown', handleUserKeyPress);
             socket.on('messageToClient', (msg) => {
                setData({...msg});
+               if(!isDesktop()) endgame = msg.endGameStatus ? true : false;
             });
           });
           socket.on('disconnect', () => {
